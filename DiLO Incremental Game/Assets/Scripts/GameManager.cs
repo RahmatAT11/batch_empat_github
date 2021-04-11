8a4d9bcf82506ed9e8ac63f48c32382a64250354
Rahmat_Alrafi_Trikada <63140592+RahmatAT11@users.noreply.github.com>
2021-04-11 08:25:51 +0700

    Membuat sistem Auto-Collect

---
+++ DiLO Incremental Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance = null;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    // Fungsi [Range(min, max)] ialah menjaga value agar tetap berada di antara min dan maxnya
+    [Range(0f, 1f)]
+    public float AutoCollectPercentage = 0.1f;
+    public ResourceConfig[] ResourceConfigs;
+
+    public Transform ResourceParent;
+    public ResourceController ResourcePrefab;
+
+    public Text GoldInfo;
+    public Text AutoCollectInfo;
+
+    private List<ResourceController> _activeResources = new List<ResourceController>();
+    private float _collectSecond;
+
+    private double _totalGold;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void AddAllResources()
+    {
+        foreach (ResourceConfig config in ResourceConfigs)
+        {
+            GameObject obj = Instantiate(ResourcePrefab.gameObject, ResourceParent, false);
+            ResourceController resource = obj.GetComponent<ResourceController>();
+
+            resource.SetConfig(config);
+            _activeResources.Add(resource);
+        }
+    }
+
+    private void CollectPerSecond()
+    {
+        double output = 0;
+        foreach(ResourceController resource in _activeResources)
+        {
+            output += resource.GetOutput();
+        }
+
+        output *= AutoCollectPercentage;
+
+        // Fungsi ToString("F1") ialah membulatkan angka menjadi desimal yang memiliki 1 angka di belakang koma
+        AutoCollectInfo.text = $"Auto Collect: {output.ToString("F1")} / second";
+
+        AddGold(output);
+    }
+
+    private void AddGold(double value)
+    {
+        _totalGold += value;
+        GoldInfo.text = $"Gold: {_totalGold.ToString("0")}";
+    }
+}
+
+// Fungsi System.Serializable adalah agar object bisa di-serialize dan
+// value dapat di-set dari inspector
+[System.Serializable]
+public struct ResourceConfig
+{
+    public string Name;
+    public double UnlockCost;
+    public double UpgradeCost;
+    public double Output;
+}
---
+++ DiLO Incremental Game/Assets/Scripts/ResourceController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceController : MonoBehaviour
+{
+    public Text ResourceDescription;
+    public Text ResourceUpgradeCost;
+    public Text ResourceUnlockCost;
+
+    private ResourceConfig _config;
+    private int _level = 1;
+
+    public void SetConfig(ResourceConfig config)
+
+    {
+        _config = config;
+        // ToString("0") berfungsi untuk membuang angka di belakang koma
+        ResourceDescription.text = $"{ _config.Name } Lv. { _level }\n+{ GetOutput().ToString("0") }";
+        ResourceUnlockCost.text = $"Unlock Cost\n{ _config.UnlockCost }";
+        ResourceUpgradeCost.text = $"Upgrade Cost\n{ GetUpgradeCost() }";
+    }
+    
+    public double GetOutput()
+
+    {
+        return _config.Output * _level;
+    }
+
+    public double GetUpgradeCost()
+
+    {
+        return _config.UpgradeCost * _level;
+    }
+    
+    public double GetUnlockCost()
+
+    {
+        return _config.UnlockCost;
+    }
+}
