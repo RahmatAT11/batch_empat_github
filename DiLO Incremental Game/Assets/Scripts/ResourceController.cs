109a6684a19bf39002f707f6ab633f8ce049638e
Rahmat_Alrafi_Trikada <63140592+RahmatAT11@users.noreply.github.com>
2021-04-12 17:20:20 +0700

    Membuat Fitur Achievement

--- DiLO Incremental Game/Assets/Scripts/ResourceController.cs
+++ DiLO Incremental Game/Assets/Scripts/ResourceController.cs
@@ -83,6 +83,7 @@ public class ResourceController : MonoBehaviour
 
         SetUnlocked(true);
         GameManager.Instance.ShowNextResource();
+        AchievementController.Instance.UnlockAchievement(AchievementType.UnlockResource, _config.Name);
     }
 
     public void SetUnlocked(bool unlocked)
