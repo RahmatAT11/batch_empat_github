97441aa5fadfdbecf7410c7c00e1330f670a7204
Rahmat_Alrafi_Trikada <63140592+RahmatAT11@users.noreply.github.com>
2021-04-16 08:21:10 +0700

    Membuat karakter berjalan dan menggunakan physic

---
+++ 2D Endless Runner/Assets/Scripts/CharacterMoveController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// script yang mengatur seluruh kendali character yang digunakan player
+public class CharacterMoveController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveAccel;
+    public float maxSpeed;
+
+    private Rigidbody2D rig;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 velocityVector = rig.velocity;
+        velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
+
+        rig.velocity = velocityVector;
+    }
+}
