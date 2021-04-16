a2432d316da57eaad2afadeb70960a6bca361ade
Rahmat_Alrafi_Trikada <63140592+RahmatAT11@users.noreply.github.com>
2021-04-16 08:53:45 +0700

    Menggunakan raycast dan layermask untuk membuat karakter bisa melompat

--- 2D Endless Runner/Assets/Scripts/CharacterMoveController.cs
+++ 2D Endless Runner/Assets/Scripts/CharacterMoveController.cs
@@ -17,6 +17,10 @@ public class CharacterMoveController : MonoBehaviour
     private bool isJumping;
     private bool isOnGround;
 
+    [Header("Ground Raycast")]
+    public float groundRaycastDistance;
+    public LayerMask groundLayerMask;
+
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
@@ -36,9 +40,36 @@ public class CharacterMoveController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // raycast ground
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundRaycastDistance, groundLayerMask);
+        if (hit)
+        {
+            if (!isOnGround && rig.velocity.y <= 0)
+            {
+                isOnGround = true;
+            }
+        }
+        else
+        {
+            isOnGround = false;
+        }
+
+        // mengkalkulasi kecepatan vector
         Vector2 velocityVector = rig.velocity;
+
+        if (isJumping)
+        {
+            velocityVector.y += jumpAccel;
+            isJumping = false;
+        }
+
         velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
 
         rig.velocity = velocityVector;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(transform.position, transform.position + (Vector3.down * groundRaycastDistance), Color.white);
     }
 }
