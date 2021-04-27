45ad962d3cabde4f33c37da0ff4deec3113b3d5d
Rahmat_Alrafi_Trikada <63140592+RahmatAT11@users.noreply.github.com>
2021-04-27 18:44:33 +0700

    Membuat enemy dan variannya

---
+++ DiLO Tower Defense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private int _maxHealth = 1;
+    [SerializeField] private float _moveSpeed = 1f;
+    [SerializeField] private SpriteRenderer _healthBar;
+    [SerializeField] private SpriteRenderer _healthFill;
+
+    private int _currentHealth;
+
+    // Fungsi ini terpanggil sekali setiap kali menghidupkan game object yang memiliki script ini
+    private void OnEnable()
+    {
+        _currentHealth = _maxHealth;
+        _healthFill.size = _healthBar.size;
+    }
+}
