743d517a659313a30e4b2fb0e01a8dc81da51708
Rahmat_Alrafi_Trikada <63140592+RahmatAT11@users.noreply.github.com>
2021-04-27 19:43:39 +0700

    Enemy mengikuti jalur

--- DiLO Tower Defense/Assets/Scripts/Enemy.cs
+++ DiLO Tower Defense/Assets/Scripts/Enemy.cs
@@ -11,10 +11,64 @@ public class Enemy : MonoBehaviour
 
     private int _currentHealth;
 
+    public Vector3 TargetPosition { get; private set; }
+    public int CurrentPathIndex { get; private set; }
+
     // Fungsi ini terpanggil sekali setiap kali menghidupkan game object yang memiliki script ini
     private void OnEnable()
     {
         _currentHealth = _maxHealth;
         _healthFill.size = _healthBar.size;
+    }
+
+    public void MoveToTarget()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, TargetPosition, _moveSpeed * Time.deltaTime);
+    }
+
+    public void SetTargetPosition(Vector3 targetPosition)
+    {
+        TargetPosition = targetPosition;
+        _healthBar.transform.parent = null;
+
+        // Mengubah rotasi dari enemy
+        Vector3 distance = TargetPosition - transform.position;
+        if (Mathf.Abs(distance.y) > Mathf.Abs(distance.x))
+        {
+            // Menghadap atas
+            if (distance.y > 0)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 90f));
+            }
+
+            // Menghadap bawah
+            else
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, -90f));
+            }
+        }
+
+        else
+        {
+            // Menghadap kanan
+            if (distance.x > 0)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+            }
+
+            // Menghadap kiri
+            else
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 180f));
+            }
+        }
+
+        _healthBar.transform.parent = transform;
+    }
+
+    // Menandai indeks terakhir pada path
+    public void SetCurrentIndexPath(int currentIndex)
+    {
+        CurrentPathIndex = currentIndex;
     }
 }
--- DiLO Tower Defense/Assets/Scripts/LevelManager.cs
+++ DiLO Tower Defense/Assets/Scripts/LevelManager.cs
@@ -21,12 +21,62 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private Transform _towerUIParent;
     [SerializeField] private GameObject _towerUIPrefab;
     [SerializeField] private Tower[] _towerPrefabs;
+    [SerializeField] private Enemy[] _enemyPrefabs;
+
+    [SerializeField] private Transform[] _enemyPaths;
+    [SerializeField] private float _spawnDelay = 5f;
 
     private List<Tower> _spawnedTowers = new List<Tower>();
+    private List<Enemy> _spawnedEnemies = new List<Enemy>();
+
+    private float _runningSpawnDelay;
 
     private void Start()
     {
         InstantiateAllTowerUI();
+    }
+
+    private void Update()
+    {
+        // Counter untuk spawn enemy dalam jeda waktu yang ditentukan
+        // Time.unscaledDeltaTime adalah deltaTime yang independent, tidak terpengaruh oleh apapun kecuali game object itu sendiri,
+        // jadi bisa digunakan sebagai penghitung waktu
+
+        _runningSpawnDelay -= Time.unscaledDeltaTime;
+        if (_runningSpawnDelay <= 0f)
+        {
+            SpawnEnemy();
+            _runningSpawnDelay = _spawnDelay;
+        }
+
+        foreach (Enemy enemy in _spawnedEnemies)
+        {
+            if (!enemy.gameObject.activeSelf)
+            {
+                continue;
+            }
+
+            // Kenapa nilainya 0.1? Karena untuk lebih mentoleransi perbedaan posisi,
+            // akan terlalu sulit jika perbedaan posisinya harus 0 atau sama persis
+            if (Vector2.Distance(enemy.transform.position, enemy.TargetPosition) < 0.1f)
+            {
+                enemy.SetCurrentIndexPath(enemy.CurrentPathIndex + 1);
+                if (enemy.CurrentPathIndex < _enemyPaths.Length)
+                {
+                    enemy.SetTargetPosition(_enemyPaths[enemy.CurrentPathIndex].position);
+                }
+
+                else
+                {
+                    enemy.gameObject.SetActive(false);
+                }
+            }
+
+            else
+            {
+                enemy.MoveToTarget();
+            }
+        }
     }
 
     // Menampilkan seluruh Tower yang tersedia pada UI Tower Selection
@@ -47,5 +97,41 @@ public class LevelManager : MonoBehaviour
     public void RegisterSpawnedTower(Tower tower)
     {
         _spawnedTowers.Add(tower);
+    }
+
+    private void SpawnEnemy()
+    {
+        int randomIndex = Random.Range(0, _enemyPrefabs.Length);
+        string enemyIndexString = (randomIndex + 1).ToString();
+
+        GameObject newEnemyObj = _spawnedEnemies.Find(
+            e => !e.gameObject.activeSelf && e.name.Contains(enemyIndexString))?.gameObject;
+
+        if (newEnemyObj == null)
+        {
+            newEnemyObj = Instantiate(_enemyPrefabs[randomIndex].gameObject);
+        }
+
+        Enemy newEnemy = newEnemyObj.GetComponent<Enemy>();
+        if (!_spawnedEnemies.Contains(newEnemy))
+        {
+            _spawnedEnemies.Add(newEnemy);
+        }
+
+        newEnemy.transform.position = _enemyPaths[0].position;
+        newEnemy.SetTargetPosition(_enemyPaths[1].position);
+        newEnemy.SetCurrentIndexPath(1);
+        newEnemy.gameObject.SetActive(true);
+    }
+
+    // Untuk menampilkan garis penghubung dalam window Scene
+    // tanpa harus di-Play terlebih dahulu
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < _enemyPaths.Length - 1; i++)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawLine(_enemyPaths[i].position, _enemyPaths[i + 1].position);
+        }
     }
 }
